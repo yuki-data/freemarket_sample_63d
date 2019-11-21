57e712b1d58ae4e266a5cb718d5055707b50854c
Kotaro-Kitafusa <puppet0pierrot@gmail.com>
2019-11-21 13:54:10 +0900

    unicorn gem install, create production.rb

--- Gemfile
+++ Gemfile
@@ -60,3 +60,11 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+#####################################
+####### Unicornを本番環境に追加 ########
+#####################################
+
+group :production do
+  gem 'unicorn', '5.4.1'
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,12 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+
+#####################################################################
+######### JSのテンプレートリテラル機能を使用するのでuglifierはオフに　########
+#####################################################################
+
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
