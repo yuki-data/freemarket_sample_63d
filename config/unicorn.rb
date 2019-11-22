e18be382b0b6c603b3f3a16edfede209bd48fbaf
Kotaro-Kitafusa <puppet0pierrot@gmail.com>
2019-11-22 21:03:45 +0900

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,7 +10,7 @@ working_directory app_path
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
-#ポート番号を指定
+#ポート番号を指
 listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
