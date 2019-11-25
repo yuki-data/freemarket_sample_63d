5aff50420e3c149aee968b0784d02c580d8bc141
Kotaro-Kitafusa <puppet0pierrot@gmail.com>
2019-11-25 20:56:38 +0900

    create cwavetests controller, Model, table, routing

---
+++ app/models/cwavetest.rb
@@ -0,0 +1,2 @@
+class Cwavetest < ApplicationRecord
+end
---
+++ test/controllers/cwavetests_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CwavetestsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
