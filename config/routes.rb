718d2f26338c2e011bf9f6c6e182e70f90aebed2
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-27 17:25:25 +0900

    add routing to card registation form page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: "tets#index"
   resources :exhibit
   resources :user_accounts, only: [:index], path: "mypage"
-  resources :card_registrations, only: [:index], path: "/mypage/card"
+  resources :card_registrations, only: [:index, :new], path: "/mypage/card"
 end
