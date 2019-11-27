80b8dfd8f53da5b55450dc170bc061f98ac74e03
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-27 14:20:44 +0900

    set temporary routing for card registration page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :cwavetests, only: [:index, :create, :new]
   resources :exhibit
   resources :user_accounts, only: [:index], path: "mypage"
+  get "/mypage/card", to: "user_accounts#card"
 end
