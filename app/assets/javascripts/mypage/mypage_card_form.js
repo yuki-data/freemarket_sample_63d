15ec19395c37c1892101854a2b1c51ef15d2571e
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-28 20:50:04 +0900

    add JS to show tool-tip

---
+++ app/assets/javascripts/mypage/mypage_card_form.js
@@ -0,0 +1,14 @@
+$(function() {
+  var creditCardToolTip = $(
+    ".mypage__card__form__security-code__about__tool-tip"
+  );
+
+  $(".mypage__card__form__security-code__about").click(function(e) {
+    creditCardToolTip.toggle();
+    e.stopPropagation();
+  });
+
+  $(".mypage").click(function() {
+    creditCardToolTip.hide();
+  });
+});
