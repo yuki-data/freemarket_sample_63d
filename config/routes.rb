ca9f8b359556dc55b9ec6368c37923f5cc7bd9a8
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-11-29 13:13:18 +0900

    add poducts#show for products details page

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :exhibit
   resources :user_accounts, only: [:index], path: "mypage"
   resources :card_registrations, only: [:index, :new], path: "/mypage/card"
+  resources :products, only: [:show]
 end
