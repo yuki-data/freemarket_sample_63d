e0fc483c518ea53d6850ed801359978c42524b11
boiledshRimp <ochiaitakahashi2@gmail.com>
2019-12-01 15:04:10 +0900

    20191201 confirmation done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :tops, only: [:index]
+  resources :tops, only: [:index, :show]
   resources  :cwavetests, only: [:index, :create, :new]
   resources :exhibit
 end
