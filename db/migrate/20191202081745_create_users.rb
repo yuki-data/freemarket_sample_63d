33d4dcf98928ab71e9ac8953c1e1b7ff6b811445
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-02 17:48:02 +0900

    add columns to users table

--- db/migrate/20191202081745_create_users.rb
+++ db/migrate/20191202081745_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :nickname, null: false
+      t.text :profile, null: false
+      t.string :avator
       t.timestamps
     end
   end
