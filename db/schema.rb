2729af9349e01783725a1615ef0900b80062a274
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-03 15:24:41 +0900

    add reference to categories on products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_061600) do
+ActiveRecord::Schema.define(version: 2019_12_03_062242) do
 
   create_table "blands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 2019_12_03_061600) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
@@ -89,5 +91,6 @@ ActiveRecord::Schema.define(version: 2019_12_03_061600) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
 end
