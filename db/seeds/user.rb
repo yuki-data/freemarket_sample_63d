e66a0f2b413e79a8776467417a740885b2454d58
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-03 19:20:12 +0900

    add seeds file for users data

---
+++ db/seeds/user.rb
@@ -0,0 +1,7 @@
+User.create!(
+  [
+    { nickname: "山田", profile: "こんにちは", avator: "山田の画像" },
+    { nickname: "田中", profile: "こんにちは", avator: "田中の画像" },
+    { nickname: "佐藤", profile: "はじめまして", avator: "佐藤の画像" }
+  ]
+)
