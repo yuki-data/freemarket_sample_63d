34b17e1d760783746d85b1088cfe289120e0de22
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-03 17:35:24 +0900

    rename boughat to bought

--- app/models/bought_product.rb
+++ app/models/bought_product.rb
@@ -1,4 +1,4 @@
-class BoughatProduct < ApplicationRecord
+class BoughtProduct < ApplicationRecord
   belongs_to :product
   belongs_to :user
 end
--- test/models/bought_product_test.rb
+++ test/models/bought_product_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class BoughatProductTest < ActiveSupport::TestCase
+class BoughtProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
