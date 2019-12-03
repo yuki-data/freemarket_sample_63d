2e214c119bb67bd76e761486abca5781d30b1ce8
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-03 17:39:39 +0900

    change association of product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  belongs_to :saling_product
-  belongs_to :bought_product
-  belongs_to :sold_product
+  has_one :saling_product
+  has_one :bought_product
+  has_one :sold_product
   belongs_to :brand
   has_many :product_images
   has_many :comments
