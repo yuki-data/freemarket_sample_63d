e84f47c3beba42ea09cfd89f2e563a2c0809f52e
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-03 15:15:34 +0900

    remove category id from products table
    rails generate migration RemoveCategoryIdFromProducts category_id:integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_060046) do
+ActiveRecord::Schema.define(version: 2019_12_03_061339) do
 
   create_table "blands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 2019_12_03_060046) do
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "category_id", null: false
     t.integer "brand_id"
     t.text "description", null: false
     t.integer "status", null: false
