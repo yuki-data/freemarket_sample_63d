783636372d59d8db412dcc3dc4d89f7ceb299d20
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-05 13:05:58 +0900

    migrate user_profile model

--- db/migrate/20191205033704_create_user_profiles.rb
+++ db/migrate/20191205033704_create_user_profiles.rb
@@ -1,7 +1,9 @@
 class CreateUserProfiles < ActiveRecord::Migration[5.2]
   def change
     create_table :user_profiles do |t|
-
+      t.string :nickname, null: false
+      t.text :profile, null: false
+      t.string :avator
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_114750) do
+ActiveRecord::Schema.define(version: 2019_12_05_033704) do
 
   create_table "cwavetests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.text "profile", null: false
+    t.string "avator"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
