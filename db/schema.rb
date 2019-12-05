f16da61aa516fac5f078d0dc3f98140a9d18a25b
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-05 13:49:39 +0900

    migrate brand model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_044116) do
+ActiveRecord::Schema.define(version: 2019_12_05_044609) do
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
