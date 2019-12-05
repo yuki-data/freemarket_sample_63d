0315bd21a1af5369fa4ca07bf9377d9beb279199
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-05 17:13:52 +0900

    add seed files to show show to create product model

---
+++ db/seeds/sell_product.rb
@@ -0,0 +1,17 @@
+user = UserProfile.create!(nickname: "山田", profile: "こんにちは", avator: "山田の画像")
+brand = Brand.create!(name: "シャネル")
+category = Category.create!(name: "腕時計")
+
+product = Product.create!(
+  name: "腕時計（中古品）",
+  description: "セイコーの時計です。電池切れです。",
+  status: 1,
+  who_charge_shipping: 1,
+  way_of_shipping: 1,
+  shipping_region: 1,
+  how_long_shipping: 1,
+  price: 3900,
+  brand_id: brand.id,
+  category_id: category.id,
+  user_profile_id: user.id
+)
