c5cc2a095e180dda0223d5c79e6c531297e93aab
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-05 13:52:26 +0900

    generate product model
    rails g model product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,2 @@
 class Product < ApplicationRecord
-  belongs_to :user
-  belongs_to :category
-  belongs_to :saling_product
-  belongs_to :bought_product
-  belongs_to :sold_product
-  belongs_to :brand
-  has_many :product_images
-  has_many :comments
 end
