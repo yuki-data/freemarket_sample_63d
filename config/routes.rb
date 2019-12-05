07bbf8c173ca3f82295cf2d854e170322fcd3621
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-05 13:13:20 +0900

    generate user model by devise
    rails g devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "tops#index"
   resources :tops, only: [:index, :show, :edit]
   resources :cwavetests, only: [:index, :create, :new]
