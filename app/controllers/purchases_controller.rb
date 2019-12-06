2889eca1305e6aa0006ae490a0ff5a5072e00a38
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-06 20:07:51 +0900

    add error catches to detect types of payjp errors

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,10 +4,24 @@ class PurchasesController < ApplicationController
 
   def pay
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
-    Payjp::Charge.create(
-      amount: 300, # 決済する値段
-      card: params['payjp-token'],
-      currency: 'jpy'
-    )
+    begin
+      charge = Payjp::Charge.create(
+        amount: 300, # 決済する値段
+        card: params['payjp-token'],
+        currency: 'jpy'
+      )
+    rescue Payjp::CardError => e
+      redirect_back(fallback_location: root_path, alert: 'カードが無効であるか支払額が限度を超えています')
+    rescue Payjp::InvalidRequestError => e
+      redirect_back(fallback_location: root_path, alert: '入力に間違いがあります')
+    rescue Payjp::AuthenticationError => e
+      redirect_back(fallback_location: root_path, alert: '認証に失敗しました')
+    rescue Payjp::APIConnectionError => e
+      redirect_back(fallback_location: root_path, alert: '通信に失敗しました')
+    rescue Payjp::PayjpError => e
+      redirect_back(fallback_location: root_path, alert: '決済に失敗しました。窓口に問い合わせてください。')
+    rescue => e
+      redirect_back(fallback_location: root_path, alert: '決済に失敗しました。窓口に問い合わせてください。')
+    end
   end
 end
