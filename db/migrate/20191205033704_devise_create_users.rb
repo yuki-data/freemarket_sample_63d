8349826cc33b51aaa75abd4f2a3deadabbcbc3d3
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-06 14:14:38 +0900

    swap foreign key relation between users and user_profiles

--- db/migrate/20191205033704_devise_create_users.rb
+++ db/migrate/20191205033704_devise_create_users.rb
@@ -7,7 +7,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
-      t.references :user_profile, foreign_key: true
       t.string :first_name, null: false
       t.string :last_name, null: false
       t.string :first_name_kana, null: false
--- db/migrate/20191205041244_create_user_profiles.rb
+++ db/migrate/20191205041244_create_user_profiles.rb
@@ -1,6 +1,7 @@
 class CreateUserProfiles < ActiveRecord::Migration[5.2]
   def change
     create_table :user_profiles do |t|
+      t.references :user, foreign_key: true
       t.string :nickname, null: false
       t.text :profile
       t.string :avator
