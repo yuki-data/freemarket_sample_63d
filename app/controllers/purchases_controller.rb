b48029463622d7713faa598fe5b52c6712e38c5b
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-06 16:45:50 +0900

    add pay action to submit data to payjp

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,13 @@
 class PurchasesController < ApplicationController
-  def new
+  def index
+  end
+
+  def pay
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    Payjp::Charge.create(
+      amount: 300, # 決済する値段
+      card: params['payjp-token'],
+      currency: 'jpy'
+    )
   end
 end
