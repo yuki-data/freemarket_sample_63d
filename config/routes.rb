d2b73f84ed5dd5ca9ef66dd2cd5e11768cae9849
Kotaro Kitafusa <puppet0pierrot@gmail.com>
2019-12-06 17:01:09 +0900

    routing for signup/steps, editing singup_controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,13 @@ Rails.application.routes.draw do
   resources :user_accounts, only: [:index], path: "mypage"
   resources :card_registrations, only: [:index, :new], path: "/mypage/card"
   resources :products, only: [:show]
+  resources :signup, only: [] do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4'
+      get 'done'
+    end
+  end
 end
