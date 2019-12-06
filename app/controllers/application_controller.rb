9adcad702719d432cbc739dc269c7c894f6a2823
Kotaro Kitafusa <puppet0pierrot@gmail.com>
2019-12-06 14:45:05 +0900

    change association, new.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :year_of_birth, :month_of_birth, :day_of_birth, :phone_number])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  belongs_to :user_profile
+  has_one :user_profile
   with_options presence: true do
-    validates :first_name
+  validates :first_name
     validates :last_name
     validates :first_name_kana
     validates :last_name_kana
@@ -15,4 +14,6 @@ class User < ApplicationRecord
     validates :day_of_birth
     validates :phone_number
   end
+  #ユーザー登録時に複数のテーブルに保存できるように親=>子へネストさせる(この記述はあとで逆になる)
+  accepts_nested_attributes_for :user_profile
 end
