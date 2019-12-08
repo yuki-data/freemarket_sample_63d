f2cf327c6918f044091d8362b25ae8ba2ac426f8
M.Kazu <55425356+MurakamiKazutaka@users.noreply.github.com>
2019-12-08 15:50:15 +0900

    edit_routes

--- config/routes.rb
+++ config/routes.rb
@@ -30,4 +30,11 @@ Rails.application.routes.draw do
       end
     end
   end
+  resources :product do
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+      get 'get_shipping_method'
+    end
+  end
 end
