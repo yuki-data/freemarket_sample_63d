4821a9757d8f71acdb940d46543eef9fe2d46fc7
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-08 13:38:45 +0900

    change routing of devise to use custom controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations',
+    sessions:      'users/sessions',
+  }
   root to: "tops#index"
   resources :tops, only: [:index, :show, :edit]
   resources :cwavetests, only: [:index, :create, :new]
