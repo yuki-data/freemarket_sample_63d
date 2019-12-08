e286c11a21ecde22cb15a4f5c03ddb488d96533a
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-08 18:59:04 +0900

    change routing for logout page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
   resources :tops, only: [:index, :show, :edit]
   resources :cwavetests, only: [:index, :create, :new]
   resources :exhibit
-  resources :user_accounts, only: [:index], path: "mypage"
+  resources :user_accounts, only: [:index], path: "mypage" do
+    collection do
+      get :logout
+    end
+  end
   resources :card_registrations, only: [:index, :new], path: "/mypage/card"
   resources :products, only: [:show] do
     resources :purchases, only: [:index] do
