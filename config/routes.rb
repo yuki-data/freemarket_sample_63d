02c5ef2d7628f3a88574fb770e2ed90eb5453796
boiledshRimp <ochiaitakahashi2@gmail.com>
2019-12-09 00:52:08 +0900

    userprofile_done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "tops#index"
   resources :tops, only: [:index, :show, :edit]
+  resources :userprofile, only: [:edit]
+  resources :user_registration_one, only: [:new]
+  resources :user_registration_two, only: [:new]
+  resources :user_registration_seven, only: [:new]
   resources :cwavetests, only: [:index, :create, :new]
   resources :exhibit
   resources :user_accounts, only: [:index], path: "mypage"
