ec47d5a7e7bb96e2b1f67bb2b7f221c5e3084443
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-08 20:00:54 +0900

    include FactoryBot methods as global methods

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  # FactoryBotを使用するための設定
+  config.include FactoryBot::Syntax::Methods
 end
