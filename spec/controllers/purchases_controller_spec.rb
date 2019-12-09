041a6450d58e12eca8ee0f9d012276266bcb22dd
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-09 16:32:10 +0900

    add rspec test for PurchasesController#index

--- spec/controllers/purchases_controller_spec.rb
+++ spec/controllers/purchases_controller_spec.rb
@@ -2,11 +2,42 @@ require 'rails_helper'
 
 describe PurchasesController do
   describe 'GET #index' do
-    it "renders the :index template" do
-      product = build(:product)
-      binding.pry
-      # get :index
-      # expect(response).to render_template :new
+    let(:user) { create(:user) }
+    let(:product) { create(:product) }
+    context "ログインしていない場合" do
+      it "ログインページにリダイレクトする" do
+        get :index, params: { product_id: product.id }
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+    context "ログインしている場合" do
+      before do
+        login user
+      end
+      context "商品が売り切れの場合" do
+        before do
+          buyer = create(:user_profile)
+          product.buy_product(buyer)
+          get :index, params: { product_id: product.id }
+        end
+
+        it "商品詳細ページにリダイレクトする" do
+          expect(response).to redirect_to product_path(product.id)
+        end
+      end
+      context "商品が出品中の場合（売り切れていない場合）" do
+        before do
+          get :index, params: { product_id: product.id }
+        end
+
+        it "render #index" do
+          expect(response).to render_template :index
+        end
+
+        it "@productが定義されている" do
+          expect(assigns(:product)).to eq product
+        end
+      end
     end
   end
 end
