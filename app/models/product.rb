145493b36daa45f5a585284670955e90f92b8187
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-10 17:51:07 +0900

    add enum to map integer to string for various products statuses

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,6 +12,27 @@ class Product < ApplicationRecord
     validates :price
   end
 
+  enum status: {
+    "新品、未使用": 0,
+    "未使用に近い": 1,
+    "目立った汚れや傷なし": 2,
+    "やや汚れや傷あり": 3,
+    "傷や汚れあり": 4,
+    "全体的に状態が悪い": 5
+  }
+
+  enum who_charge_shipping: {
+    "送料込み（出品者負担）": 0, "着払い（購入者負担）": 1
+  }
+
+  enum way_of_shipping: {
+    "未定": 0, "らくらくメルカリ便": 1, "ゆうメール": 2, "レターパック": 3
+  }
+
+  enum how_long_shipping: {
+    "1-2日で発送": 0, "2-3日で発送": 1, "4-7日で発送": 2
+  }
+
   belongs_to :user_profile
   belongs_to :category
   has_one :saling_product, dependent: :destroy
