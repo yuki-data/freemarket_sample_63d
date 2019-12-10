a1b256ab3c8ca5269d203c286af7878c2161645f
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-10 12:31:55 +0900

    migrate: add ancestry column to category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_060124) do
+ActiveRecord::Schema.define(version: 2019_12_10_033032) do
 
   create_table "bought_products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "product_id"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2019_12_05_060124) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
