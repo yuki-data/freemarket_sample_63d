b6908d895b46114296c50e802bf20db6e9fceda4
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-11 15:15:17 +0900

    format js file
    way of shipping form filed を追加するためのjsファイルを編集するためにフォーマットした

--- app/assets/javascripts/exhibit/shipping.js
+++ app/assets/javascripts/exhibit/shipping.js
@@ -1,38 +1,46 @@
 var $;
 
-$(document).on('turbolinks:load', function(){
+$(document).on("turbolinks:load", function() {
+  function appendShippingOption(method) {
+    var html = `<option value='${method}'>${method}</option>`;
+    return html;
+  }
 
-    function appendShippingOption(method){
-      var html = `<option value='${method}'>${method}</option>`;
-      return html
-    }
+  $("#shipping-method").on("change", function(e) {
+    e.preventDefault();
+    var exhibitorShippingMethod = [
+      "---",
+      "未定",
+      "らくらくメルカリ便",
+      "ゆうメール",
+      "レターパック",
+      "普通郵便（定型、定型外）",
+      "クロネコヤマト",
+      "ゆうパック",
+      "クリックポスト",
+      "ゆうパケット"
+    ];
+    var buyerShippingMethod = ["ゆうメール", "クロネコヤマト", "ゆうパック"];
 
-  $('#shipping-method').on('change',function(e){
-    e.preventDefault()
-    var exhibitorShippingMethod = ['---', '未定','らくらくメルカリ便','ゆうメール','レターパック','普通郵便（定型、定型外）',
-    'クロネコヤマト','ゆうパック','クリックポスト','ゆうパケット']
-    var buyerShippingMethod = ['ゆうメール','クロネコヤマト','ゆうパック']
+    var shippingParentCategory = "";
 
-    var shippingParentCategory = ""
-
-    shippingParentCategory = document.getElementById('shipping-method').value;
-    if (shippingParentCategory !='---'){
+    shippingParentCategory = document.getElementById("shipping-method").value;
+    if (shippingParentCategory != "---") {
       $.ajax({
-        url:'/product/get_shipping_method',
-        type:'GET',
-        data: {parent_name: shippingParentCategory},
-        dataType: 'json'
-      })
-
-      .done(function(method){
-        var methodGroupHtml = '';
-        var insertHTML = '';
-        if (shippingParentCategory == "送料込み（出品者負担）"){
-          exhibitorShippingMethod.forEach(function(method){
+        url: "/product/get_shipping_method",
+        type: "GET",
+        data: { parent_name: shippingParentCategory },
+        dataType: "json"
+      }).done(function(method) {
+        var methodGroupHtml = "";
+        var insertHTML = "";
+        if (shippingParentCategory == "送料込み（出品者負担）") {
+          exhibitorShippingMethod.forEach(function(method) {
             insertHTML += appendShippingOption(method);
           });
-        }else (shippingParentCategory == "着払い（購入者負担）");{
-          buyerShippingMethod.forEach(function(method){
+        } else shippingParentCategory == "着払い（購入者負担）";
+        {
+          buyerShippingMethod.forEach(function(method) {
             insertHTML += appendShippingOption(method);
           });
         }
@@ -49,13 +57,11 @@ $(document).on('turbolinks:load', function(){
                               <ul class = 'has-error-text'>選択してください</ul>
                             </div>
                           </div>`;
-      $('.way_of_shipping').append(methodGroupHtml);
+        $(".way_of_shipping").append(methodGroupHtml);
         console.log(methodGroupHtml);
-
-      })
-    }
-    else{
-      $('#shipping-method').remove();
+      });
+    } else {
+      $("#shipping-method").remove();
     }
   });
 });
