bd2c2918eb54f82573ed17f3fd4333ab585071ff
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-12 13:43:28 +0900

    remove unnecessary tables

--- db/schema.rb
+++ db/schema.rb
@@ -127,11 +127,6 @@ ActiveRecord::Schema.define(version: 2019_12_10_033032) do
     t.index ["user_id"], name: "index_user_profiles_on_user_id"
   end
 
-  create_table "user_registration_fives", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
