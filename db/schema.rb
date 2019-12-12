c8df22cabac810aedec911b67f4abb9a0e5c5535
boiledshRimp <ochiaitakahashi2@gmail.com>
2019-12-12 17:35:11 +0900

    product_list_display

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_110335) do
+ActiveRecord::Schema.define(version: 2019_12_12_052943) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -103,6 +103,11 @@ ActiveRecord::Schema.define(version: 2019_12_10_110335) do
     t.index ["buyer_id"], name: "index_sold_products_on_buyer_id"
     t.index ["product_id"], name: "index_sold_products_on_product_id"
     t.index ["user_profile_id"], name: "index_sold_products_on_user_profile_id"
+  end
+
+  create_table "tops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
