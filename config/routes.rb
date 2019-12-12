e3777cac9307a0ae5fc594078fb240000a5031c7
Kotaro Kitafusa <puppet0pierrot@gmail.com>
2019-12-13 08:25:00 +0900

    delete old user_registration

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,6 @@ Rails.application.routes.draw do
   root to: "tops#index"
   resources :tops, only: [:index, :show, :edit]
   resources :userprofile, only: [:edit]
-  resources :user_registration_one, only: [:new]
-  resources :user_registration_two, only: [:new]
-  resources :user_registration_three, only: [:new]
-  resources :user_registration_four, only: [:new]
-  resources :user_registration_five, only: [:new]
-  resources :user_registration_comp_of_regi, only: [:new]
-  resources :user_registration_seven, only: [:new]
   resources :cwavetests, only: [:index, :create, :new]
   resources :exhibit
   resources :user_accounts, only: [:index], path: "mypage" do
