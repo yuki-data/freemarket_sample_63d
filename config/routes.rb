ff3cd62b73a4a822afb199e10c273d897e779337
Kotaro Kitafusa <puppet0pierrot@gmail.com>
2019-12-12 09:26:18 +0900

    delete signup2-4 in routing

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,6 @@ Rails.application.routes.draw do
   resources :signup, only: [:create] do
     collection do
       get 'step1'
-      get 'step2'
-      get 'step3'
-      get 'step4'
-      get 'done'
     end
   end
 end
