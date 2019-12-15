d5d7ef12c644add2c6541e2d7b73928d6b7f423c
Kotaro Kitafusa <puppet0pierrot@gmail.com>
2019-12-15 17:37:34 +0900

    setting routing, controller for signup

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,11 +1,13 @@
 class SignupController < ApplicationController
   protect_from_forgery
-  def step1
-    @user = User.new
-    @user.build_user_profile
-  end
 
-  def signup_test
+  #今後セッションを設定する場合に使う為残しておきます。
+    # def step1
+    #   @user = User.new
+    #   @user.build_user_profile
+    # end
+
+  def signup
     @user = User.new
     @user.build_user_profile
   end
@@ -14,23 +16,22 @@ class SignupController < ApplicationController
       @user = User.new(user_params)
       @user.build_user_profile(user_params[:user_profile_attributes])
     if @user.save
-      # binding.pry
-      # mypageを表示できるようにする（ユーザー情報をDBからもってきて、ここのユーザーページ）
-      # コントローラ上でリダイレクトじょうけんぶんき
-      # user_signed_inしてたら遷移させる
-      # 表示確認できたら条件分岐をしてcurrent_user_idとそのmypageのidが一緒か参照させる
-      sign_in User.find(@user.id) unless user_signed_in?
-      # redirect_to auto_login_signup_index_path
+      sign_in(User.find(@user.id), scope: :user) unless user_signed_in?
       redirect_to user_accounts_path
+      # 下記は今後セッション使う場合に必要な為残してます。
+      # session[:id] = @user.id
+      # redirect_to auto_login_signup_index_path(user_id: @user.id)
     else
       redirect_to root_path
     end
   end
 
-  def auto_login
-    # binding.pry
-    sign_in User.find(12) unless user_signed_in?
-  end
+  #セッション用にこのアクションは残しておく。
+    # def auto_login
+    #   # sign_in User.find(session[:id]) unless user_signed_in?
+    #   # sign_in User.find(params[:user_id]) unless user_signed_in?
+    #   # redirect_to user_accounts_path
+    # end
 
   private
   def user_params
