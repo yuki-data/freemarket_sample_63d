9ada042493a6edb94df63395207522f141d7fbf4
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-16 14:45:52 +0900

    remove product_images.build

--- app/controllers/clone_products_controller.rb
+++ app/controllers/clone_products_controller.rb
@@ -1,6 +1,10 @@
 class CloneProductsController < ApplicationController
   def new
     @product = Product.new
-    5.times { @product.product_images.build }
+    # 5.times { @product.product_images.build }
+  end
+
+  def create
+    binding.pry
   end
 end
