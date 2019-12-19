43ce5264cff8e128368c2383d30665a2836d29df
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-12-19 15:05:03 +0900

    give product_params to new in create action since save method does not properly handle with product_params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,24 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :category_select_function, only: [:new, :edit]
-  before_action :set_new_product, only: [:new, :create]
   before_action :set_existing_product, only: [:show, :edit, :update]
 
   def show
   end
 
   def new
+    @product = Product.new
     @product.product_images.build
   end
 
   def create
+    @product = Product.new(product_params)
     unless extract_product_images.size > 0
       flash[:exhibit_notice] = "画像は１枚以上必要です"
       redirect_to new_product_path and return
     end
 
-    if @product.save(product_params)
+    if @product.save
       extract_product_images.each do |image|
         @product.product_images.create(image: image, product_id: @product.id)
       end
@@ -93,10 +94,6 @@ class ProductsController < ApplicationController
   end
 
   private
-
-  def set_new_product
-    @product = Product.new
-  end
 
   def set_existing_product
     @product = Product.find(params[:id])
