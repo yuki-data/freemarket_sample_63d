9ebc2fe38cedceb6ec718d8a68e50eb349ee7d25
Kotaro Kitafusa <puppet0pierrot@gmail.com>
2019-12-20 13:47:54 +0900

    set strong_params for user_address

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -10,12 +10,13 @@ class SignupController < ApplicationController
   def new
     @user = User.new
     @user.build_user_profile
+    @user.build_user_address
   end
 
   def create
-    binding.pry
       @user = User.new(user_params)
       @user.build_user_profile(user_params[:user_profile_attributes])
+      @user.build_user_address(user_params[:user_address_attributes])
     if @user.save
       sign_in(User.find(@user.id), scope: :user) unless user_signed_in?
       redirect_to root_path
@@ -47,6 +48,7 @@ class SignupController < ApplicationController
       :birth_day,
       :phone_number,
       user_profile_attributes: [:id, :nickname]
+      user_address_attributes: [:id, :first_name, :first_name_kana, :last_name, :last_name_kana, :post_number, :prefecture, :city, :house_number, :building_name, :phone_number]
     )
   end
 
